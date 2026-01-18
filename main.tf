4241df69b8b2e25623c692b00e9accc7ac7fa286
Kamal <40554278+knmsoutlook@users.noreply.github.com>
2026-01-18 10:17:54 -0600

    main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "EC2-from-GitHub-Actions"
+  }
+}
